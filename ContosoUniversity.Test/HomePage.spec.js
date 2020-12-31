49b2d8f6eb44e53cc4e54c1aeaab4a7d19e9f79e
fabricam <fabricam@outlook.com>
2020-12-30 23:12:09 -0500

    added test project with basic cypress test

---
+++ ContosoUniversity.Test/HomePage.spec.js
@@ -0,0 +1,9 @@
+﻿context('Connectors', () => {
+    beforeEach(() => {
+        cy.visit('https://example.cypress.io/commands/connectors')
+    })
+
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
+})
