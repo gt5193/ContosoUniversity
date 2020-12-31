8721bd27f3d46927f80fcffd06526e25db5b6d86
fabricam <fabricam@outlook.com>
2020-12-31 13:25:30 -0500

    added Xunit & basic test class

---
+++ ContosoUniversity.Test/MyTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ContosoUniversity.Test
+{
+    
+    public class MyTests
+    {
+        [Fact]
+        public void DoesNothing()
+        {
+            Assert.True(true);
+        }
+    }
+}
