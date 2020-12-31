c16635a66adfb9ad4b5572f640f731b2b78f640e
fabricam <fabricam@outlook.com>
2020-12-30 19:25:53 -0500

    removed comment

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -16,7 +16,6 @@ namespace ContosoUniversity
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            //Some random comment
         }
 
         public IConfiguration Configuration { get; }
