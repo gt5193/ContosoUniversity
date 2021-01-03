294c2591e7a7b485752ef608f3f89f2364da3bee
fabricam <fabricam@outlook.com>
2021-01-02 20:19:06 -0500

    added cypress

---
+++ ContosoUniversity.Test/cypress/integration/MyTest.spec.js
@@ -0,0 +1,8 @@
+﻿describe('My First Test', () => {
+    it('Hits a website', () => {
+        cy.visit('www.google.com')
+    })
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
+})
