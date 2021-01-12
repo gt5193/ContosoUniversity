96e3cdef3bf37b1b97da0b3b15d571142f97a53a
fabricam <fabricam@outlook.com>
2021-01-12 18:13:11 -0500

    added console app

---
+++ ContosoUniversity.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContosoUniversity.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
