f3697eff5c760ae65f5799d199df02b06f137200
fabricam <fabricam@outlook.com>
2021-01-12 20:51:06 -0500

    example using swagger generated client

--- ContosoUniversity.Service/Program.cs
+++ ContosoUniversity.Service/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ContosoUniversity.Api.Client;
 
 namespace ContosoUniversity.Service
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new WeatherForecastClient("https://localhost:44339", new System.Net.Http.HttpClient());
+            var weather = await client.GetAsync();
+            foreach (var result in weather)
+            {
+                Console.WriteLine(result.TemperatureF + "F");
+            }
+            Console.ReadLine();
         }
+        
     }
 }
